de82897acac2990f2397ef9bcf4b607fce0176f3
LefterDev <developerredox@gmail.com>
2024-02-22 22:32:14 +0200

    Covered undefined base cases

--- src/routes/apiAdminEndpoint.ts
+++ src/routes/apiAdminEndpoint.ts
@@ -4,10 +4,12 @@ export const adminEndpoint: Router = Router();
 
 adminEndpoint.post("/create-preregister", async (req: Request, res: Response) => {
     const {username, password} = req.body;
+    if(!username || !password) return res.status(305).send({error: "Missing argument(s)"});
     return createAdminKey(username, password, res);
 })
 
 adminEndpoint.post("/create-account", async (req: Request, res: Response) => {
     const {username, email} = req.body;
+    if(!username || !email) return res.status(305).send({error: "Missing argument(s"})
     return createAdminAccount(username, email, res);
 })
